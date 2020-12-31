1fbe3b5df32ad383f5a4776dd11524fe6e99d8dc
Dany Itani <ditani@berkeley.edu>
2020-12-31 14:01:36 -0800

    add paragraph intro to home page

--- src/components/HomeSection.js
+++ src/components/HomeSection.js
@@ -10,11 +10,24 @@ function HomeSection() {
       <div className="centered_cursive">
         <h1>Dany Itani</h1>
       </div>
+      <div className="centered_cursive">
+        <img src={fun_image} alt="" width="550" height="300" />
+      </div>
       <div className="centered_cambria">
         <h2>Passionate Software Engineer</h2>
       </div>
-      <div className="centered_cursive">
-        <img src={fun_image} alt="" width="550" height="300" />
+      <div>
+        <p className="paragraph_format">
+          Hello! Thank you for visiting my website!
+        </p>
+        <p className="paragraph_format">
+          A little about me! I am currently studying computer science with a
+          minor in data science at the University of California, at Berkeley. I
+          love all things related to APIs, data/machine structures, and writing
+          efficient algorithms. I also love not only making inferences from
+          data, but equally as much love deducing information by creating models
+          for decision making, statistical inference, or prediction.
+        </p>
       </div>
     </div>
   );
