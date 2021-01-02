dbf6bda1af17b3e62c1825dbffc80403d7e4c0e0
Dany Itani <ditani@berkeley.edu>
2021-01-01 17:53:40 -0800

    fix text-color issues

--- src/components/HomeSection.js
+++ src/components/HomeSection.js
@@ -1,6 +1,4 @@
 import React from "react";
-import "../App.css";
-import "./HomeSection.css";
 import "./Home.css";
 import fun_image from "../images/fun_image.png";
 // import { Svg, Defs, Rect, Mask, Circle } from 'react-native-svg';
@@ -23,27 +21,30 @@ function HomeSection() {
   return (
     <div className="background">
       <div className="centered_cursive">
-        <h1>Dany Itani</h1>
+        <h1>About: Dany Itani</h1>
       </div>
       <div className="centered_cursive">
         <img src={fun_image} alt="" width="550" height="300" />
       </div>
       <div className="centered_cambria">
-        <h2>Passionate Software Engineer</h2>
+        <h2>New Grad Software Engineer</h2>
       </div>
-      <div>
-        <p className="paragraph_format">
-          Hello! Thank you for visiting my website!
-        </p>
-        <p className="paragraph_format">
-          A little about me! I am currently studying computer science with a
-          minor in data science at the University of California, at Berkeley. I
-          love all things related to APIs, data/machine structures, and writing
-          efficient algorithms. I also love not only making inferences from
-          data, but equally as much love deducing information by creating models
-          for decision making, statistical inference, or prediction.
-        </p>
+      <div className="centered_cambria">
+        <h2>University of California, Berkeley (May, 2021)</h2>
       </div>
+      <p className="paragraph_format">
+        Hello! Thank you for visiting my website!
+      </p>
+      <p className="paragraph_format">
+        A little about me! I am currently studying computer science with a minor
+        in data science at the University of California, at Berkeley. I love all
+        things related to APIs, data/machine structures, and writing efficient
+        algorithms. I also truly enjoy not only making inferences from data, but
+        also deducing information by creating models for decision making,
+        statistical inference, or prediction. In my free time I enjoy playing
+        League of Legends, playing basketball, going on runs, and talking to my
+        mom!
+      </p>
     </div>
   );
 }
